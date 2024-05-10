221ec5193b13085e586d439f349da6e3228b94b7
chapidzes <chapidzes@yahoo.com>
2024-05-10 05:38:04 -0700

    forth commit

---
+++ day 4/homework.py
@@ -0,0 +1,11 @@
+name = input("whats your name: ")
+age = int(input("whats your age: "))
+future_years = int(input("how many years in to the future do you want to travel in time: "))
+
+print(age + future_years)
+
+
+num1 = int(input("please enter your first number: "))
+num2 = int(input("please enter your second number: "))
+
+print(num1 + num2)
