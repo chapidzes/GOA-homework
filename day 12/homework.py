9abcdf2170c473b830182f39bbfee59a4d8b634a
chapidzes <chapidzes@yahoo.com>
2024-05-21 12:09:05 -0700

    sixth commit

---
+++ day 12/homework.py
@@ -0,0 +1,23 @@
+for i in range(11):
+    if i % 2 != 0:
+        print(i)
+    
+
+
+for i in range(1, 51):
+    if i % 5 == 0:
+        print(i)
+
+
+num = int(input("enter an integer: "))
+for i in range(num + 1):
+    print(i)
+
+
+total = 0
+
+for i in range(51):
+    print(i)
+    total += i
+print("sum of the numbers:",total)
+
---
+++ day 14/homework.py
@@ -0,0 +1,10 @@
+print("for loop:")
+for i in range(0, 51, 5 )
+     print(i)
+
+
+print("while loop:")
+num = 0
+while num <= 50:
+     print(num)
+     num += 5
